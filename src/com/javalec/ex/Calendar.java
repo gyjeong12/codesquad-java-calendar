140aab51648ee8a3d012a43486924db3eaaed667
Gun Yong Jeong <gyjeong@gmail.com>
2018-11-11 17:13:39 -0800

    Update using class method

--- src/com/javalec/ex/Calendar.java
+++ src/com/javalec/ex/Calendar.java
@@ -3,22 +3,33 @@ package com.javalec.ex;
 import java.util.Scanner;
 
 public class Calendar {
-
-	public static void main(String[] args) {
+	
+	private final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
+	public void printSampleCalendar() {
 		System.out.println("Mon Tue Wed Thur Fri Sat Sun");
 		System.out.println("----------------------------");
 		System.out.println("1   2   3    4   5   6   7");
 		System.out.println("8   9   10   11  12  13  14");
 		System.out.println("15  16  17   18  19  20  21");
 		System.out.println("22  23  24   25  26  27  28");
-		
+	}
+
+	public static void main(String[] args) {		
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("Enter Month : ");
 		int month = scanner.nextInt();
 		
 		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
 		
-		System.out.printf("%d month have %d days \n",month, maxDays[month-1]);
+		System.out.printf("%d month have %d days \n\n",month, cal.getMaxDaysOfMonth(month));
+		
+		cal.printSampleCalendar();
 		scanner.close();
 	}
 
